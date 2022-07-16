20e645d0b78f35c0dd316cd893beb05e7a7426db
Mohit Panjikar <98936526+Mohitpanjikar@users.noreply.github.com>
2022-07-16 23:32:26 +0530

    Add files via upload

---
+++ remove-element.cpp
@@ -0,0 +1,29 @@
+// https://youtu.be/KXofHSQuYMw
+// https://leetcode.com/problems/remove-element/submissions/
+/*Example 1:
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores). */
+
+class Solution
+{
+public:
+  int removeElement (vector < int >&nums, int val)
+  {
+    vector < int >::iterator it;
+      it = nums.begin ();
+    for (int i = 0; i < nums.size (); i++)
+      {
+	if (nums[i] == val)
+	  {
+	    nums.erase (it);
+	    it--;
+	    i--;
+	  }
+	it++;
+      }
+    return nums.size ();
+  }
+};
